d3aa95d1b3eeef74f5a7fa6e908a98cdfb9648be
David Allen <davetvmusic@hotmail.com>
2020-07-24 16:42:19 +1000

    add comments

--- src/app/custom-control/custom-control.component.ts
+++ src/app/custom-control/custom-control.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit, forwardRef } from '@angular/core';
+import { Component, forwardRef } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
+// extend the ng value accessor (with custom provider)
+// provide == gain access to existing value accessor
+// useExisting == pass our component
+// forwardRef == hoist/ wait for omponent to become available
+// multi: true == means we are extending NG_VALUE_ACCESSOR with our component
 const EMAIL_CONTROL_ACCESSOR = {
 	provide: NG_VALUE_ACCESSOR,
 	useExisting: forwardRef(() => CustomControlComponent),
@@ -21,13 +26,17 @@ export class CustomControlComponent implements ControlValueAccessor {
 	private onTouch: Function;
 	private onModelChange: Function;
 
+	// bind to functions that angular reactive forms are exposing and assign to internal function
 	registerOnTouched(fn) {
+		// notify formControl that it has been interacted with
 		this.onTouch = fn;
 	};
 	registerOnChange(fn) {
+		// used to update the form model when values propagate from the view to the model.
 		this.onModelChange = fn;
 	};
 
+	// receive the value from formControl that is bound to CustomControlComponent
 	writeValue(value) {
 		this.value = value;
 	};
